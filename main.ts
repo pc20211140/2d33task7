61edc57352a0a032ed780c1cc9e1c2ff3eb7616b
pc20211140 <115052204+pc20211140@users.noreply.github.com>
2022-11-09 09:25:18 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.lightLevel())
 })
+input.onButtonPressed(Button.B, function () {
+    if (input.lightLevel() <= 100) {
+        led.setBrightness(255)
+    }
+})
 basic.forever(function () {
 	
 })
