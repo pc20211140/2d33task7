8cc3742fea4b19d38055de384ecf2aac2809e024
pc20211140 <115052204+pc20211140@users.noreply.github.com>
2022-11-09 09:29:40 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.lightLevel())
 })
-input.onButtonPressed(Button.B, function () {
-    led.plotBarGraph(
-    input.lightLevel(),
-    255
-    )
-})
 basic.forever(function () {
 	
 })
