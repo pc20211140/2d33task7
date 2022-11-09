22b5094e8433ad88cf42b5290c256e76ffd9e6f1
pc20211140 <115052204+pc20211140@users.noreply.github.com>
2022-11-09 09:28:39 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,9 +2,10 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.lightLevel())
 })
 input.onButtonPressed(Button.B, function () {
-    if (input.lightLevel() <= 100) {
-        led.setBrightness(255)
-    }
+    led.plotBarGraph(
+    input.lightLevel(),
+    255
+    )
 })
 basic.forever(function () {
 	
